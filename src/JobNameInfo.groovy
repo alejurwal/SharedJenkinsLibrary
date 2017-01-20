b2a33ff36f370c6312ae45660de7cf85f27b4524
Alex Taylor <alexanderrtaylor@gmail.com>
2017-01-20 15:51:18 -0500

    Update JobNameInfo.groovy

--- src/JobNameInfo.groovy
+++ src/JobNameInfo.groovy
@@ -7,17 +7,17 @@ class JobNameInfo{
     this.remotePath = remotePathParam;
     this.jobUrl = jobURLParam;
   }
-
-  /**Get the remote path for trigger remote and copy artifact remote*/
-  def getRemotePath(){
-    return remotePath;
-  }
-  /**Get the jenkins job url */
-  def getJobURL(){
+def getJobURL(){
     return jobURL;
   }
-  /**GEt the short job name without folder full path */
-  /*def getShortName(){
+  /**Get the remote path for trigger remote and copy artifact remote*/
+  /*def getRemotePath(){
+    return remotePath;
+  }
+  //Get the jenkins job url
+  
+  //GEt the short job name without folder full path
+  def getShortName(){
     int index = remotePath.lastIndexOf("/");
     if(index != -1){
       return remotePath.substring(index);
