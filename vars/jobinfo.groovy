9e3b95bacd9317f754833f74341adf0fb526ee6d
Alex Taylor <alexanderrtaylor@gmail.com>
2017-01-20 15:00:34 -0500

    Update jobinfo.groovy

--- vars/jobinfo.groovy
+++ vars/jobinfo.groovy
@@ -2,33 +2,7 @@ import groovy.transform.Field;
 
 
 //Class for the object for JobInfo.groovy
-@NonCPS
-class JobNameInfo{
-  String remotePath;
-  String jobUrl;
-  JobNameInfo(String remotePath, String jobURL){
-    remotePath = remotePath;
-    jobURL = jobURL;
-  }
 
-  /**Get the remote path for trigger remote and copy artifact remote*/
-  String getRemotePath(){
-    return remotePath;
-  }
-  /**Get the jenkins job url */
-  String getJobURL(){
-    return jobURL;
-  }
-  /**GEt the short job name without folder full path */
-  String getShortName(){
-    int index = remotePath.lastIndexOf("/");
-    if(index != -1){
-      return remotePath.substring(index);
-    }else{
-      return remotePath;
-    }
-  }
-}
 
 /**This file holds global string constants for jobs in jenkins
 Contains the remotejob path, url and shortname in the JobNameInfo class
