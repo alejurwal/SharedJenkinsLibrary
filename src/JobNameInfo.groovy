5af64df7389630e3252c48333ccb482b02a029a6
Alex Taylor <alexanderrtaylor@gmail.com>
2017-05-02 10:02:43 -0400

    Update JobNameInfo.groovy

--- src/JobNameInfo.groovy
+++ src/JobNameInfo.groovy
@@ -1,30 +1,7 @@
 
-//Class for the object for JobInfo.groovy
-class JobNameInfo implements Serializable {
-  string remotePath;
-  string jobUrl;
   
   @NonCPS
   def echoSomeThings(def Variable1, def Variable2){
     echo '${Variable1}'
     echo '${Variable2}'
-  }
-  //TESTING COmmit
-  //Get the remote path for trigger remote and copy artifact remote
-  /*@NonCPS
-  def getRemotePath(){
-    return this.remotePath;
-  }
-  //Get the jenkins job url
-  
-  //GEt the short job name without folder full path
-  @NonCPS
-  def getShortName(){
-    int index = remotePath.lastIndexOf("/");
-    if(index != -1){
-      return remotePath.substring(index);
-    }else{
-      return remotePath;
-    }
-  }*/
 }
