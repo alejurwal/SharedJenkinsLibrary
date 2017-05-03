e51afb7b21ff5d31503723fa68cfd0cf874b8094
Alex Taylor <alexanderrtaylor@gmail.com>
2017-05-03 09:41:16 -0400

    Update Variables.groovy

--- Variables.groovy
+++ Variables.groovy
@@ -1,2 +1,26 @@
-def Check1 = 'Fake'
-def Check2 = 'Fake2'
+appID = "wibac"
+appName = "accounts"
+appComponentVersionKeys = "accounts_appVersion"
+appReleaseFileSystemRoot = "/workerfs/release/OBP/ACCOUNTS/"
+approvalEnvironments = "QA_MT2"
+prodEnvironments = "QA_MT2"
+
+def getAppID() {
+    return appID;
+}
+
+def getAppName() {
+    return appName;
+}
+def getAppComponentVersionKeys() {
+    return appComponentVersionKeys
+}
+def appReleaseFileSystemRoot() {
+    return appReleaseFileSystemRoot
+}
+def approvalEnvironments() {
+    return approvalEnvironments
+}
+def prodEnvironments() {
+    return prodEnvironments
+}
