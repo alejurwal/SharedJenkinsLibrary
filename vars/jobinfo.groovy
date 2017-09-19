f7ed252a4400d773bcfd2a765878354a3064d99b
Alex Taylor <alexanderrtaylor@gmail.com>
2017-09-19 09:58:38 -0400

    Update jobinfo.groovy

--- vars/jobinfo.groovy
+++ vars/jobinfo.groovy
@@ -4,9 +4,3 @@ def setPCFDeployApiFUN(def Array){
 def getPCFDeployApiFUN(def Array){
     name
 }
-def call(def Variable1, def Variable2){
-    echo Variable1
-    echo Variable2
-    Variable3 = "foo"
-    def Variable4 = "bar"
-}
