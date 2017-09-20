0aef832dba1983e70904fe40f13e811ed0f3e2fd
Alex Taylor <alexanderrtaylor@gmail.com>
2017-09-20 17:02:42 -0400

    Update jobinfo.groovy

--- vars/jobinfo.groovy
+++ vars/jobinfo.groovy
@@ -1,28 +1,7 @@
-appID = "wibac"
-appName = "accounts"
-appComponentVersionKeys = "accounts_appVersion"
-appReleaseFileSystemRoot = "/workerfs/release/OBP/ACCOUNTS/"
-approvalEnvironments = "QA_MT2"
-prodEnvironments = "QA_MT2"
-
-def getAppID() {
-    return appID;
+def setName(value) {
+    privateName = value
+    echo "Setting name to ${privateName}"
 }
-
-def getAppName() {
-    return appName;
+def getName() {
+    privateName
 }
-def getAppComponentVersionKeys() {
-    return appComponentVersionKeys
-}
-def appReleaseFileSystemRoot() {
-    return appReleaseFileSystemRoot
-}
-def approvalEnvironments() {
-    return approvalEnvironments
-}
-def prodEnvironments() {
-    return prodEnvironments
-}
-
-return this;
