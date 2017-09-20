69dbed0150131010d9f19ca9dfc9e9967a091a10
Alex Taylor <alexanderrtaylor@gmail.com>
2017-09-20 16:57:57 -0400

    Update jobinfo.groovy

--- vars/jobinfo.groovy
+++ vars/jobinfo.groovy
@@ -1,4 +1,28 @@
-def call(body) { 
-  def pcfDeployApiFUN = ['ORG': 'FUN', 'SPACE': 'webapi']
+appID = "wibac"
+appName = "accounts"
+appComponentVersionKeys = "accounts_appVersion"
+appReleaseFileSystemRoot = "/workerfs/release/OBP/ACCOUNTS/"
+approvalEnvironments = "QA_MT2"
+prodEnvironments = "QA_MT2"
+
+def getAppID() {
+    return appID;
 }
+
+def getAppName() {
+    return appName;
+}
+def getAppComponentVersionKeys() {
+    return appComponentVersionKeys
+}
+def appReleaseFileSystemRoot() {
+    return appReleaseFileSystemRoot
+}
+def approvalEnvironments() {
+    return approvalEnvironments
+}
+def prodEnvironments() {
+    return prodEnvironments
+}
+
 return this;
