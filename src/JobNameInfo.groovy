b639fcd265ba510be68a75f58f070172af39d399
Alex Taylor <alexanderrtaylor@gmail.com>
2017-10-03 15:56:24 -0400

    Update JobNameInfo.groovy

--- src/JobNameInfo.groovy
+++ src/JobNameInfo.groovy
@@ -1,7 +1,13 @@
-
-  
-  @NonCPS
-  def echoSomeThings(def Variable1, def Variable2){
-    echo '${Variable1}'
-    echo '${Variable2}'
+@Grab('com.github.groovy-wslite:groovy-wslite:1.1.3')
+import wslite.http.auth.*;
+import wslite.rest.*;
+import wslite.soap.*;
+import wslite.*;
+def client = new SOAPClient('http://www.holidaywebservice.com/Holidays/US/Dates/USHolidayDates.asmx')
+def response = client.send(SOAPAction:'http://www.27seconds.com/Holidays/US/Dates/GetMothersDay') {
+    body {
+        GetMothersDay('xmlns':'http://www.27seconds.com/Holidays/US/Dates/') {
+            year(2011)
+        }
+    }
 }
