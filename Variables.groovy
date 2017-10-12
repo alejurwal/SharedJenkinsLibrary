9a7fe1df9d7b3d022aca3caf67eac31e8d1caa25
Alex Taylor <alexanderrtaylor@gmail.com>
2017-10-12 14:57:24 -0400

    Update Variables.groovy

--- Variables.groovy
+++ Variables.groovy
@@ -4,7 +4,6 @@ appComponentVersionKeys = "accounts_appVersion"
 appReleaseFileSystemRoot = "/workerfs/release/OBP/ACCOUNTS/"
 approvalEnvironments = "QA_MT2"
 prodEnvironments = "QA_MT2"
-prodEnvironments3 = "QA_MT3"
 
 def getAppID() {
     return appID;
