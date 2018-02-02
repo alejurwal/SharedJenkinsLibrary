b249d6ddc5c400bb335990b5a52bfb48ef588988
Alex Taylor <alexanderrtaylor@gmail.com>
2018-02-02 12:48:57 -0500

    Update JobNameInfo.groovy

--- src/JobNameInfo.groovy
+++ src/JobNameInfo.groovy
@@ -1,13 +1,10 @@
-@Grab('com.github.groovy-wslite:groovy-wslite:1.1.3')
-import wslite.http.auth.*;
-import wslite.rest.*;
-import wslite.soap.*;
-import wslite.*;
-def client = new SOAPClient('http://www.holidaywebservice.com/Holidays/US/Dates/USHolidayDates.asmx')
-def response = client.send(SOAPAction:'http://www.27seconds.com/Holidays/US/Dates/GetMothersDay') {
-    body {
-        GetMothersDay('xmlns':'http://www.27seconds.com/Holidays/US/Dates/') {
-            year(2011)
-        }
-    }
+#!/bin/groovy
+import org.wb.*
+
+//Map allBuilds = [:]
+builds = [:]
+
+def call()
+{
+    def build = new standardBuild()
 }
