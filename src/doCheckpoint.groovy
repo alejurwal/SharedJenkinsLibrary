763e091802bec003da65fd9a5bb0f95ff0e28aaf
Alex Taylor <alexanderrtaylor@gmail.com>
2018-05-02 12:24:13 -0400

    Update doCheckpoint.groovy

--- src/doCheckpoint.groovy
+++ src/doCheckpoint.groovy
@@ -1,3 +1,10 @@
-def call() {
-  checkpoint 'IamCheckpoint'
+def call( ) { 
+try { 
+sh 'echo Hello' 
+sh 'echo Hii' 
+} catch (Exception err) { 
+currentBuild.result = 'FAILED' 
+throw new RuntimeException(err) 
+} 
+echo "End of the build" 
 }
