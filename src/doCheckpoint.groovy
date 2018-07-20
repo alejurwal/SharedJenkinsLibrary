3f831e349166fd8f168f98b519cc73700e3ee837
Alex Taylor <alexanderrtaylor@gmail.com>
2018-07-20 09:29:23 -0400

    Update doCheckpoint.groovy

--- src/doCheckpoint.groovy
+++ src/doCheckpoint.groovy
@@ -1,10 +1,14 @@
-def call( ) { 
-try { 
-sh 'echo Hello' 
-sh 'echo Hii' 
-} catch (Exception err) { 
-currentBuild.result = 'FAILED' 
-throw new RuntimeException(err) 
-} 
-echo "End of the build" 
+class Utilities implements Serializable {
+  def steps
+  Utilities(steps) {this.steps = steps}
+  def shell() {
+    steps.withCredentials([[$class: 'UsernamePasswordMultiBinding',credentialsId: '10dedc95-ba8a-4ffe-bca6-8dea9586c8dc', 
+    usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) 
+    { 
+      steps.sh "echo $USERNAME:$PASSWORD"
+      steps.sh "echo '$USERNAME:$PASSWORD'"
+      steps.sh "echo '${USERNAME}:${PASSWORD}'"
+    }
+  }
 }
+
