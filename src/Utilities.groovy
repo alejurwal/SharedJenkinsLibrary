08855c64ab6eae21365dddf5f43ba4c2923d0e31
Alex Taylor <alexanderrtaylor@gmail.com>
2018-07-20 09:44:47 -0400

    Update Utilities.groovy

--- src/Utilities.groovy
+++ src/Utilities.groovy
@@ -5,8 +5,8 @@ class Utilities implements Serializable {
     steps.withCredentials([[$class: 'UsernamePasswordMultiBinding',credentialsId: '10dedc95-ba8a-4ffe-bca6-8dea9586c8dc', 
     usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) 
     { 
-      steps.sh "echo $USERNAME:$PASSWORD"
-      steps.sh "echo '$USERNAME:$PASSWORD'"
+      //steps.sh "echo $USERNAME:$PASSWORD"
+      //steps.sh "echo '$USERNAME:$PASSWORD'"
       steps.sh "echo '${USERNAME}:${PASSWORD}'"
     }
   }
